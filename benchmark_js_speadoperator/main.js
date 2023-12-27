7331909c3685d42a5b941cd5c5b9f0bc69270ce8
Alex <jk1143298@gmail.com>
2023-12-27 23:24:39 +0100

    trace_memory

--- benchmark_js_speadoperator/main.js
+++ benchmark_js_speadoperator/main.js
@@ -1,21 +1,36 @@
+const { trace_memory } = require('../lib/trace_memory')
 
+// > node --stack-size=65500 main.js
 
-function _js_spreadoperator(n, ...arguments) {
+let N = 1000
+
+function _js_spreadoperator(n, ...args) {
   // console.log(n)
-  if(n===10000) {
+  if(n===N) {
+    // process.stdin()
     return n
   }
-  return _js_spreadoperator(n+1, ...arguments)
+  return _js_spreadoperator(n+1, ...args)
 }
 
-function js_spreadoperator(n, arguments) {
-  if(n===10000) {
+function js_spreadoperator(n, args) {
+  if(n===N) {
     return n
   }
-  return js_spreadoperator(n+1, arguments)
+  return js_spreadoperator(n+1, args)
   
 }
 
+function trace_spread(...args) {
+  trace_memory(() => {
+    _js_spreadoperator(0, ...args)
+  }, { verbose: true, unit: 'MB' } )
+
+  trace_memory(() => {
+    js_spreadoperator(0, args)
+  }, { verbose: true, unit: 'MB', fix_decimal: 2 })
+
+}
 
 (function main(argc, argv) {
   let args = []
@@ -23,10 +38,14 @@ function js_spreadoperator(n, arguments) {
     args.push(i)
 
   let start = Date.now()
+
+  trace_spread(...args)
   // RangeError: Maximum call stack size exceeded
   // TODO: WHY?
-  // let x = _js_spreadoperator(0,...args)
-  let x = js_spreadoperator(0, args)
-  console.log( x )
+
+  // let x = _js_spreadoperator(0, ...args)
+  // let x = js_spreadoperator(0, args)
+  // console.log( x )
   console.log(Date.now() - start)
+
 })(process.argv.length, process.argv)
